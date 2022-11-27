546eccfd3290a8d13185bcf4fcc6f2f5126a1edb
MohammedShoaib <shoaib.addu@gmail.com>
2022-11-27 00:04:49 -0800

    bug fixes

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -294,8 +294,9 @@ lock_release (struct lock *lock)
           int max_priority = 0;
           struct list_elem *e;
           for (e = list_begin (&holder->locks_i_hold); e != list_end (&holder->locks_i_hold); e = list_next (e)) {
-              struct lock temp_lock = *e; //TODO: review this
-              struct semaphore *sem = &temp_lock->semaphore;
+              struct lock *temp_lock = list_entry (e, struct lock, lock_elem);
+              struct *lock temp_lock = e;
+              struct semaphore *sem = temp_lock->semaphore;
 
               struct thread *t = list_entry (list_front (&sem->waiters), struct thread, elem);
               int temp_priority = t->priority;
