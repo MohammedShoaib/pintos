daff188a38bbc14858e6b5ccd76df14bd512f72b
MohammedShoaib <shoaib.addu@gmail.com>
2022-12-11 15:50:28 -0800

    adding test script

---
+++ src/userprog/test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+make build/tests/userprog/args-none.result
+make build/tests/userprog/args-single.result
+make build/tests/userprog/args-multiple.result
+make build/tests/userprog/args-many.result
+make build/tests/userprog/args-dbl-space.result
+make build/tests/userprog/halt.result
+make build/tests/userprog/exit.result
