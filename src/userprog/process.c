9d635dd71239b2511f68ab90bc16341da0c65e3e
MohammedShoaib <shoaib.addu@gmail.com>
2022-12-11 20:00:22 -0800

    removig lock

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -68,9 +68,7 @@ start_process (void *file_name_)
   if_.gs = if_.fs = if_.es = if_.ds = if_.ss = SEL_UDSEG;
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
-  lock_acquire(&file_system_lock);
   success = load (file_name, &if_.eip, &if_.esp, &saveptr);
-  lock_release(&file_system_lock);
     /* If load failed, quit. */
   palloc_free_page (file_name);
     if (!success){
