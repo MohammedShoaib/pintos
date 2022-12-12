0c1015021b87ba6a9fcdbda2d0106eecfd3f8901
MohammedShoaib <shoaib.addu@gmail.com>
2022-12-11 16:41:03 -0800

    Revert "adding extra credit tests and file lock"
    This reverts commit f99caf3d8a99f29b5b8aec8adffd46766b538dfb.

--- src/userprog/test.sh
+++ src/userprog/test.sh
@@ -6,10 +6,3 @@ make build/tests/userprog/args-many.result
 make build/tests/userprog/args-dbl-space.result
 make build/tests/userprog/halt.result
 make build/tests/userprog/exit.result
-make build/tests/userprog/open-normal.result
-make build/tests/userprog/open-missing.result
-make build/tests/userprog/open-boundary.result
-make build/tests/userprog/open-empty.result
-make build/tests/userprog/open-null.result
-make build/tests/userprog/open-bad-ptr.result
-make build/tests/userprog/open-twice.result
